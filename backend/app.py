915edcf9a3fef4eab47d60b0656c79da6c78429b
BarutJozef <xbarut@stuba.sk>
2024-04-07 21:45:58 +0200

    updating app, new model, new requirements files

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,18 @@
 import base64
+import torch
 
-from tensorflow import keras
 from flask import Flask, request
 from io import BytesIO
 from matplotlib import pyplot
-from ex_2_flowers.dataset_initializer import DatasetInitializer
-from ex_2_flowers.utils import generate_con_fake_samples
+from deep_learning.main_model.models import Encoder, Generator
 
 app = Flask(__name__)
-model = keras.models.load_model('trained_models/flowers_generator_model_100.h5')
-tokenizer = DatasetInitializer.initialize_tokenizer("backend/all_descriptions.txt")
+device = 'cpu'
+model = Generator()
+state_dict = torch.load('trained_models/flowers_generator_final', map_location=device)
+model.load_state_dict(state_dict)
+model.eval()
+
 
 @app.after_request
 def after_request(response):
@@ -17,17 +20,23 @@ def after_request(response):
     header['Access-Control-Allow-Origin'] = '*'
     return response
 
+
 @app.route("/", methods=['GET'])
 def home():
     prompt = request.args['input']
     print(prompt)
-    inputs = DatasetInitializer.process_text_input(tokenizer, [prompt] * 5, 20)
-    images, _ = generate_con_fake_samples(model, inputs, 128, len(inputs))
+    encoded_prompts = Encoder.encode_prompt([prompt] * 5, device=device)
+    encoded_prompts = torch.reshape(encoded_prompts, (*encoded_prompts.shape, 1, 1))
+    latent_vec = torch.randn(5, 100, 1, 1, device=device)
+    with torch.no_grad():
+        images = model(latent_vec, encoded_prompts)
+    images = (images + 1) / 2
     response_arr = []
+
     for image in images:
         tmp = BytesIO()
         pyplot.axis('off')
-        pyplot.imshow(image[:, :, :])
+        pyplot.imshow(image.permute(1, 2, 0))
         pyplot.savefig(tmp, format='png', bbox_inches="tight", transparent=True, pad_inches=0)
         pyplot.close()
         base64_image = base64.encodebytes(tmp.getvalue()).decode('utf-8')
